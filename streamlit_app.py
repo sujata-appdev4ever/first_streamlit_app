9fd7136892d1c83ae1ac60736fe892f469b4ba93
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-10 06:33:40 +0530

    Update streamlit_app.py
    remove the json line and edit fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just write the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 # take the json verson of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show output in the screen as table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.t
