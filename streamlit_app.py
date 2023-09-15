5dee163acb2dec914012863826d3e5d5f0ef03c9
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-15 07:19:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,17 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
+# Add a text entry box after displaying the table
+fruit_choice = streamlit.text_input('What fruit would you like to add?:', 'jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
 
 
 
