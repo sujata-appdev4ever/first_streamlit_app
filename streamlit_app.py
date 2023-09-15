c6fc5db99b00e246b604122ab511c8940675e33f
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-15 08:53:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
-
+import pandas
+import request
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
-
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -10,23 +12,18 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -39,12 +36,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-
 except URLError as e:
 streamlit.error()
 
 
-# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -52,17 +47,14 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
 # Add a text entry box after displaying the table
 fruit_choice = streamlit.text_input('What fruit would you like to add?:', 'jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
-
 
 # This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from fruit_load_lit')")
 
 
-from urllib.error import URLError
 
 
 
