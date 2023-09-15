1dd1d9c78ad94dbb5192390e6bdf8b36d080f34b
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-15 08:37:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,19 +23,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-#don't run anything past here while we troubleshoot
-streamlit.stop()
+except URLError as e:
+streamlit.error()
 
 # import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
