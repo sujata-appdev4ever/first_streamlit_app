9e29ee7e427da6d4aac7298a1a6a20f5238983ca
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-16 05:05:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error("Error occurred: " + str(e))
+streamlit.error()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
