c5fc982ed0150f6ebde885517d9e7b830c996627
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-15 07:47:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ streamlit.dataframe(my_data_rows)
 
 # Add a text entry box after displaying the table
 fruit_choice = streamlit.text_input('What fruit would you like to add?:', 'jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
 
 
 
