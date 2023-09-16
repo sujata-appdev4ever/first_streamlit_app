06196d68e26c61572464e59706a6caf925aaf318
sujata-appdev4ever <144080842+sujata-appdev4ever@users.noreply.github.com>
2023-09-16 07:21:08 +0530

    Update streamlit_app.py
    Add another button

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,12 +52,17 @@ if streamlit.button ('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
  
-# Add a text entry box after displaying the table
-fruit_choice = streamlit.text_input('What fruit would you like to add?:', 'jackfruit')
-streamlit.write('Thanks for adding')
-
-# This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from fruit_load_lit')")
+# Allow the end userto add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+return "Thanks for adding" + new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake (add_my_fruit)
+    streamlit.text(back_from_function)
 
 from urllib.error import URLError
 
